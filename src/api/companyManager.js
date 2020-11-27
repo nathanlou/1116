688db4de8de00f29a694ae371db047bdb883d3ab
zhaosong <838923588@qq.com>
2020-11-27 16:15:50 +0800

    BUG修复
    BUG修复

---
+++ src/api/companyManager.js
@@ -0,0 +1,43 @@
+import request from '@/utils/request'
+
+//信息员查询
+export function companyUserListBsysData(data) {
+  return request({
+    url: '/qygl/companyUser_listBsysData',
+    method: 'post',
+    params:data
+  })
+}
+//信息员新增/修改
+export function companyUserSave(data) {
+  return request({
+    url: '/qygl/companyUser_save',
+    method: 'post',
+    params:data
+  })
+}
+//信息员查看详情
+export function companyUserGetDetail(data) {
+  return request({
+    url: '/qygl/companyUser_getDetail',
+    method: 'post',
+    params:data
+  })
+}
+//信息员充值密码
+export function companyUserUpdateResetPd(data) {
+  return request({
+    url: '/qygl/companyUser_updateResetPd',
+    method: 'post',
+    params:data
+  })
+}
+//信息员删除
+export function companyUserDelete(data) {
+  return request({
+    url: '/qygl/companyUser_delete',
+    method: 'post',
+    params:data
+  })
+}
+
